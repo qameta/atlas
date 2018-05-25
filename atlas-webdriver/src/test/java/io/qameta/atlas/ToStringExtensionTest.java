d1d72cad75c0358701368bc155af31416180f7cf
kurau <kurau@yandex-team.ru>
2018-05-25 12:23:22 +0300

    add elements collection tests

--- atlas-webdriver/src/test/java/io/qameta/atlas/ToStringExtensionTest.java
+++ atlas-webdriver/src/test/java/io/qameta/atlas/ToStringExtensionTest.java
@@ -1,8 +1,5 @@
 package io.qameta.atlas;
 
-import io.qameta.atlas.extensions.FindBy;
-import io.qameta.atlas.extensions.FindByExtension;
-import io.qameta.atlas.extensions.Name;
 import io.qameta.atlas.extensions.ToStringExtension;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Before;
@@ -17,10 +14,6 @@ import static org.mockito.Mockito.when;
  * @author kurau (Yuri Kalinin)
  */
 public class ToStringExtensionTest {
-
-    private static final String SELECTOR = "//div";
-    private static final String ELEMENT_NAME = "my name";
-    private static final String METHOD_NAME = "childWithoutName";
 
     private String message;
     private WebElement parent;
@@ -46,40 +39,5 @@ public class ToStringExtensionTest {
                 .create(parent, AtlasWebElement.class);
 
         assertThat(atlasWebElement.toString()).isEqualTo(message);
-    }
-
-    @Test
-    public void shouldUseNameAnnotationInFindByExtension() {
-        ParentElement atlasWebElement = new Atlas()
-                .extension(new FindByExtension())
-                .extension(new ToStringExtension(message))
-                .create(parent, ParentElement.class);
-
-        assertThat(atlasWebElement.childWithName().toString()).isEqualTo(ELEMENT_NAME);
-    }
-
-    @Test
-    public void shouldUseMethodNameInFindByExtension() {
-        ParentElement atlasWebElement = new Atlas()
-                .extension(new FindByExtension())
-                .extension(new ToStringExtension(message))
-                .create(parent, ParentElement.class);
-
-        assertThat(atlasWebElement.childWithoutName().toString()).isEqualTo(METHOD_NAME);
-    }
-
-    interface ParentElement extends AtlasWebElement {
-
-        @Name(ELEMENT_NAME)
-        @FindBy(SELECTOR)
-        ChildElement childWithName();
-
-        @FindBy(SELECTOR)
-        ChildElement childWithoutName();
-
-    }
-
-    interface ChildElement extends AtlasWebElement {
-
     }
 }
