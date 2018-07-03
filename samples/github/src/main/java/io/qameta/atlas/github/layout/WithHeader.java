0a91b0e4ce9b79a37d2c85f12af715454bff3ed5
Artem Eroshenko <eroshenkoam@me.com>
2018-07-03 11:16:37 +0300

    codestyle fixes

--- samples/github/src/main/java/io/qameta/atlas/github/layout/WithHeader.java
+++ samples/github/src/main/java/io/qameta/atlas/github/layout/WithHeader.java
@@ -3,9 +3,12 @@ package io.qameta.atlas.github.layout;
 import io.qameta.atlas.extension.FindBy;
 import io.qameta.atlas.github.element.Header;
 
+/**
+ * @author Artem Eroshenko.
+ */
 public interface WithHeader {
 
     @FindBy("//header")
-    Header header ();
+    Header header();
 
 }
--- samples/github/src/main/java/io/qameta/atlas/github/page/MainPage.java
+++ samples/github/src/main/java/io/qameta/atlas/github/page/MainPage.java
@@ -3,6 +3,9 @@ package io.qameta.atlas.github.page;
 import io.qameta.atlas.WebPage;
 import io.qameta.atlas.github.layout.WithHeader;
 
+/**
+ * @author Artem Eroshenko.
+ */
 public interface MainPage extends WebPage, WithHeader {
 
 }
