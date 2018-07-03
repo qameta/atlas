74599a27fa8215aaaf861cc449230c76ed1183f7
Artem Eroshenko <eroshenkoam@me.com>
2018-07-03 11:08:45 +0300

    add first fraft of github sample

---
+++ samples/github/src/main/java/io/qameta/atlas/github/page/SearchPage.java
@@ -0,0 +1,8 @@
+package io.qameta.atlas.github.page;
+
+import io.qameta.atlas.WebPage;
+import io.qameta.atlas.github.layout.WithHeader;
+
+public interface SearchPage extends WebPage, WithHeader {
+
+}
---
+++ samples/github/src/test/java/io/qameta/atlas/github/FindRepositoryByNameTest.java
@@ -0,0 +1,14 @@
+package io.qameta.atlas.github;
+
+import org.junit.Test;
+
+public class FindRepositoryByNameTest {
+
+    @Test
+    public void simpleTest() {
+        //open main page
+        //search by repository name 'atlas'
+        //verify every item contains title with substring 'atlas'
+    }
+
+}
