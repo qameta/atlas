d6ac6743af84b2a029d89c10727f7153268f780d
Artem Eroshenko <eroshenkoam@me.com>
2018-07-03 11:16:37 +0300

    codestyle fixes

--- samples/github/src/main/java/io/qameta/atlas/github/element/Header.java
+++ samples/github/src/main/java/io/qameta/atlas/github/element/Header.java
@@ -2,8 +2,9 @@ package io.qameta.atlas.github.element;
 
 import io.qameta.atlas.AtlasWebElement;
 
+/**
+ * @author Artem Eroshenko.
+ */
 public interface Header extends AtlasWebElement {
-
-
 
 }
