8b06a6e48755d98a5b6576b4eb46d1cfc8c786ca
kurau <kurau@yandex-team.ru>
2018-07-16 19:08:40 +0300

    add parameterized find by

---
+++ atlas-webdriver/src/main/java/io/qameta/atlas/extension/Param.java
@@ -0,0 +1,17 @@
+package io.qameta.atlas.extension;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Param.
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Param {
+
+    String value();
+
+}
