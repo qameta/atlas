64f4f6d8e9c9c2d5e3830e89e984076a6bbceaeb
artsok <relix@mail.ru>
2018-11-17 13:20:32 +0300

    add logic to find method by reflection

--- atlas-core/src/main/java/io/qameta/atlas/internal/TargetMethodInvoker.java
+++ atlas-core/src/main/java/io/qameta/atlas/internal/TargetMethodInvoker.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.reflect.MethodUtils;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -18,11 +19,12 @@ public class TargetMethodInvoker implements MethodInvoker {
     @Override
     public Object invoke(final Object proxy, final MethodInfo methodInfo, final Configuration config) throws Throwable {
         final Object target = config.requireContext(TargetContext.class).getValue().instance();
-
-        final Method targetMethod = MethodUtils.getMatchingMethod(
-                target.getClass(), methodInfo.getMethod().getName(), getParametersTypes(methodInfo.getArgs()));
+        final Optional<Method> targetMethod = Optional.ofNullable(MethodUtils.getMatchingMethod(
+                target.getClass(), methodInfo.getMethod().getName(), getParametersTypes(methodInfo.getArgs())));
         try {
-            return targetMethod.invoke(target, methodInfo.getArgs());
+            return targetMethod.orElseGet(() -> MethodUtils.getAccessibleMethod(
+                    target.getClass(), methodInfo.getMethod().getName(), getParametersTypes(methodInfo.getArgs())))
+                    .invoke(target, methodInfo.getArgs());
         } catch (InvocationTargetException e) {
             throw e.getTargetException();
         }
@@ -36,5 +38,4 @@ public class TargetMethodInvoker implements MethodInvoker {
                 .map(Object::getClass)
                 .collect(Collectors.toList()).toArray(new Class<?>[]{});
     }
-
 }
