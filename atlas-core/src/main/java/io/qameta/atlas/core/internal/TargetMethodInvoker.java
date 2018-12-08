ba75740494d9415df5e11f84b6018d68caf7a306
artsok <relix@mail.ru>
2018-12-09 00:33:06 +0300

    add func to find methods add description to reflect method and turn on test

--- atlas-core/src/main/java/io/qameta/atlas/core/internal/TargetMethodInvoker.java
+++ atlas-core/src/main/java/io/qameta/atlas/core/internal/TargetMethodInvoker.java
@@ -3,13 +3,11 @@ package io.qameta.atlas.core.internal;
 import io.qameta.atlas.core.api.MethodInvoker;
 import io.qameta.atlas.core.context.TargetContext;
 import io.qameta.atlas.core.util.MethodInfo;
-import org.apache.commons.lang3.reflect.MethodUtils;
+import io.qameta.atlas.core.util.ReflectionUtils;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * Target method invoker.
@@ -19,12 +17,10 @@ public class TargetMethodInvoker implements MethodInvoker {
     @Override
     public Object invoke(final Object proxy, final MethodInfo methodInfo, final Configuration config) throws Throwable {
         final Object target = config.requireContext(TargetContext.class).getValue().instance();
-        final Optional<Method> targetMethod = Optional.ofNullable(MethodUtils.getMatchingMethod(
-                target.getClass(), methodInfo.getMethod().getName(), getParametersTypes(methodInfo.getArgs())));
+        final Method targetMethod = ReflectionUtils.getMatchingMethod(
+                target.getClass(), methodInfo.getMethod().getName(), getParametersTypes(methodInfo.getArgs()));
         try {
-            return targetMethod.orElseGet(() -> MethodUtils.getAccessibleMethod(
-                    target.getClass(), methodInfo.getMethod().getName(), getParametersTypes(methodInfo.getArgs())))
-                    .invoke(target, methodInfo.getArgs());
+            return targetMethod.invoke(target, methodInfo.getArgs());
         } catch (InvocationTargetException e) {
             throw e.getTargetException();
         }
@@ -35,7 +31,6 @@ public class TargetMethodInvoker implements MethodInvoker {
             return new Class<?>[]{};
         }
         return Arrays.stream(args)
-                .map(Object::getClass)
-                .collect(Collectors.toList()).toArray(new Class<?>[]{});
+                .map(Object::getClass).toArray(Class[]::new);
     }
 }
--- atlas-core/src/main/java/io/qameta/atlas/core/util/ReflectionUtils.java
+++ atlas-core/src/main/java/io/qameta/atlas/core/util/ReflectionUtils.java
@@ -5,10 +5,10 @@ import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.lang3.reflect.ConstructorUtils;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Reflection utils.
@@ -40,5 +40,33 @@ public final class ReflectionUtils {
         } catch (Exception e) {
             throw new AtlasException("Can't instantiate class " + clazz, e);
         }
+    }
+
+    /**
+     * Get matching methods for the given class, superclass and all of interfaces implemented by the given
+     * class and its superclasses. Note: first, find method in current class and etc.
+     *
+     * @param cls - current Class {@link Class}
+     * @param methodName - method name {@link String}
+     * @param parameterTypes - parameter types {@link Class}
+     * @return required method {@link Method}
+     */
+    public static Method getMatchingMethod(final Class<?> cls, final String methodName,
+                                           final Class<?>... parameterTypes) {
+        if (!Objects.nonNull(cls) && !Objects.requireNonNull(methodName).isEmpty()) {
+            throw new AtlasException("Null class not allowed.");
+        }
+        final Predicate<Method> filter1 = method -> methodName.equals(method.getName())
+                && Objects.deepEquals(parameterTypes, method.getParameterTypes());
+        final Predicate<Method> filter2 = method -> methodName.equals(method.getName())
+                && ClassUtils.isAssignable(parameterTypes, method.getParameterTypes(), true);
+
+        return Stream.of(
+                Collections.singletonList(cls), ClassUtils.getAllSuperclasses(cls), ClassUtils.getAllInterfaces(cls))
+                .flatMap(Collection::stream)
+                .map(Class::getDeclaredMethods)
+                .flatMap(Arrays::stream)
+                .filter(filter1.or(filter2))
+                .findFirst().orElseThrow(() -> new AtlasException("Can't find valid method"));
     }
 }
