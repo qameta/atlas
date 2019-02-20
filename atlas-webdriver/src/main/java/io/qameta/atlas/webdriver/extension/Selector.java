5eb93d6c642881b8d18430e3488d7685a29b4b93
Artem Eroshenko <eroshenkoam@me.com>
2019-02-20 14:30:06 +0300

    codestyle fixes

--- atlas-webdriver/src/main/java/io/qameta/atlas/webdriver/extension/Selector.java
+++ atlas-webdriver/src/main/java/io/qameta/atlas/webdriver/extension/Selector.java
@@ -10,59 +10,59 @@ public enum Selector {
 
     CLASS_NAME {
         @Override
-        public By buildBy(String value) {
+        public By buildBy(final String value) {
             return By.className(value);
         }
     },
     CSS {
         @Override
-        public By buildBy(String value) {
+        public By buildBy(final String value) {
             return By.cssSelector(value);
         }
     },
     ID {
         @Override
-        public By buildBy(String value) {
+        public By buildBy(final String value) {
             return By.id(value);
         }
     },
     ID_OR_NAME {
         @Override
-        public By buildBy(String value) {
+        public By buildBy(final String value) {
             return new ByIdOrName(value);
         }
     },
     LINK_TEXT {
         @Override
-        public By buildBy(String value) {
+        public By buildBy(final String value) {
             return By.linkText(value);
         }
     },
     NAME {
         @Override
-        public By buildBy(String value) {
+        public By buildBy(final String value) {
             return By.name(value);
         }
     },
     PARTIAL_LINK_TEXT {
         @Override
-        public By buildBy(String value) {
+        public By buildBy(final String value) {
             return By.partialLinkText(value);
         }
     },
     TAG_NAME {
         @Override
-        public By buildBy(String value) {
+        public By buildBy(final String value) {
             return By.tagName(value);
         }
     },
     XPATH {
         @Override
-        public By buildBy(String value) {
+        public By buildBy(final String value) {
             return By.xpath(value);
         }
     };
 
-    public abstract By buildBy(String value);
+    public abstract By buildBy(final String value);
 
 }
