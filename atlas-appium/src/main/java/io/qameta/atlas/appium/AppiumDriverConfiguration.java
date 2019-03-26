e8067e5e1c7bffd61a20dc2a06cf68e2d8769aeb
artsok <relix@mail.ru>
2019-03-26 12:50:52 +0300

    fix configurations

--- atlas-appium/src/main/java/io/qameta/atlas/appium/AppiumDriverConfiguration.java
+++ atlas-appium/src/main/java/io/qameta/atlas/appium/AppiumDriverConfiguration.java
@@ -2,10 +2,19 @@ package io.qameta.atlas.appium;
 
 import io.appium.java_client.AppiumDriver;
 import io.qameta.atlas.appium.context.AppiumDriverContext;
-import io.qameta.atlas.appium.extension.*;
+import io.qameta.atlas.appium.extension.AppiumDriverProviderExtension;
+import io.qameta.atlas.appium.extension.AppiumFindByExtension;
+import io.qameta.atlas.appium.extension.LongPressExtension;
+import io.qameta.atlas.appium.extension.SwipeDownOnExtension;
+import io.qameta.atlas.appium.extension.SwipeUpOnExtension;
 import io.qameta.atlas.core.internal.Configuration;
 import io.qameta.atlas.core.internal.DefaultMethodExtension;
-import io.qameta.atlas.webdriver.extension.*;
+import io.qameta.atlas.webdriver.extension.FilterCollectionExtension;
+import io.qameta.atlas.webdriver.extension.FindByCollectionExtension;
+import io.qameta.atlas.webdriver.extension.ShouldMethodExtension;
+import io.qameta.atlas.webdriver.extension.ToStringMethodExtension;
+import io.qameta.atlas.webdriver.extension.WaitUntilMethodExtension;
+import io.qameta.atlas.webdriver.extension.WrappedElementMethodExtension;
 
 
 /**
