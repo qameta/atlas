d32be24b036b7b727f8b07f72f9e7d47b49d0200
Artem Eroshenko <eroshenkoam@qameta.io>
2021-05-18 14:13:12 +0300

    clean up build files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,22 @@
-import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
-
-buildscript {
-    repositories {
-        jcenter()
-        mavenLocal()
-    }
-
-}
-
-val gradleScriptDir by extra("${rootProject.projectDir}/gradle")
-
-tasks.withType(Wrapper::class) {
-    gradleVersion = "6.0.1"
-}
-
 plugins {
     java
     id("ru.vyarus.quality") version "4.0.0"
-    id("com.jfrog.bintray") version "1.8.0"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
-    id("net.researchgate.release") version "2.8.1"
 }
 
-release {
-    tagTemplate = "\${version}"
-}
+description = "Atlas"
 
-configure(listOf(rootProject)) {
-    description = "Atlas"
-    group = "io.qameta.atlas"
-}
-
-val afterReleaseBuild by tasks.existing
-
-configure(subprojects) {
+allprojects {
     group = "io.qameta.atlas"
     version = version
+}
 
+configure(subprojects) {
     apply(plugin = "java")
     apply(plugin = "maven")
     apply(plugin = "java-library")
     apply(plugin = "ru.vyarus.quality")
     apply(plugin = "io.spring.dependency-management")
-
-    apply(from = "$gradleScriptDir/maven.gradle")
-    apply(from = "$gradleScriptDir/bintray.gradle")
 
     java {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -69,15 +41,7 @@ configure(subprojects) {
         (options as StandardJavadocDocletOptions).addStringOption("Xdoclint:none", "-quiet")
     }
 
-    artifacts.add("archives", sourceJar)
-    artifacts.add("archives", javadocJar)
-
-    val bintrayUpload by tasks.existing
-    afterReleaseBuild {
-        dependsOn(bintrayUpload)
-    }
-
-    configure<DependencyManagementExtension> {
+    dependencyManagement {
         dependencies {
             dependency("org.apache.commons:commons-lang3:3.7")
 
