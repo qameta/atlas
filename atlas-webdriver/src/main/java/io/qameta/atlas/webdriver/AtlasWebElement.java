dd46f9659f1e3dc58d9d0b47f950da610a9bd682
dimas7 <dimas.mbox@gmail.com>
2021-08-24 17:51:43 +0300

    move WrapsElement from org.openqa.selenium.internal to org.openqa.selenium

--- atlas-webdriver/src/main/java/io/qameta/atlas/webdriver/AtlasWebElement.java
+++ atlas-webdriver/src/main/java/io/qameta/atlas/webdriver/AtlasWebElement.java
@@ -7,7 +7,7 @@ import org.hamcrest.Matcher;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Coordinates;
 import org.openqa.selenium.interactions.Locatable;
-import org.openqa.selenium.internal.WrapsElement;
+import org.openqa.selenium.WrapsElement;
 
 import java.util.List;
 
