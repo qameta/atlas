8f0f6b16a2ac6dbae0ecff469ff77d94d9b6e1d7
Dmitrii Bogachev <bogachevdmitry.s@gmail.com>
2023-05-31 19:12:26 +0200

    Fix issue with NoSuchElement error when trying to execute default interface method on Java 9+

--- atlas-core/src/main/java/io/qameta/atlas/core/internal/DefaultMethodExtension.java
+++ atlas-core/src/main/java/io/qameta/atlas/core/internal/DefaultMethodExtension.java
@@ -2,8 +2,11 @@ package io.qameta.atlas.core.internal;
 
 import io.qameta.atlas.core.api.MethodExtension;
 import io.qameta.atlas.core.util.MethodInfo;
+import org.apache.commons.lang3.SystemUtils;
 
+import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
@@ -20,12 +23,43 @@ public class DefaultMethodExtension implements MethodExtension {
     @Override
     public Object invoke(final Object proxy, final MethodInfo methodInfo, final Configuration config) throws Throwable {
         final Class<?> declaringClass = methodInfo.getMethod().getDeclaringClass();
-        final Constructor<MethodHandles.Lookup> constructor = MethodHandles.Lookup.class
-                .getDeclaredConstructor(Class.class, int.class);
-        constructor.setAccessible(true);
-        return constructor.newInstance(declaringClass, MethodHandles.Lookup.PRIVATE)
-                .unreflectSpecial(methodInfo.getMethod(), declaringClass)
+
+        if (isJava8()) {
+            final Constructor<MethodHandles.Lookup> constructor = MethodHandles.Lookup.class
+                    .getDeclaredConstructor(Class.class, int.class);
+            constructor.setAccessible(true);
+            return constructor.newInstance(declaringClass, MethodHandles.Lookup.PRIVATE)
+                    .unreflectSpecial(methodInfo.getMethod(), declaringClass)
+                    .bindTo(proxy)
+                    .invokeWithArguments(methodInfo.getArgs());
+        }
+
+        final MethodHandles.Lookup lookup = MethodHandles.lookup();
+        final MethodHandle methodHandle = lookup.findSpecial(
+                declaringClass,
+                methodInfo.getMethod().getName(),
+                MethodType.methodType(
+                        methodInfo.getMethod().getReturnType(),
+                        methodInfo.getMethod().getParameterTypes()
+                ),
+                declaringClass
+        );
+
+        return methodHandle
                 .bindTo(proxy)
                 .invokeWithArguments(methodInfo.getArgs());
+    }
+
+    private boolean isJava8() {
+        String[] versionElements = SystemUtils.JAVA_SPECIFICATION_VERSION.split("\\.");
+        int discard = Integer.parseInt(versionElements[0]);
+        int version;
+        if (discard == 1) {
+            version = Integer.parseInt(versionElements[1]);
+        } else {
+            version = discard;
+        }
+
+        return version == 8;
     }
 }
