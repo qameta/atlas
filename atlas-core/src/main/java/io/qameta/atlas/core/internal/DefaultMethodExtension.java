b38decf9f8c53b20dd61a3fbe56f478fb66c8edb
Dmitry <bogachevdmitry.s@gmail.com>
2023-05-31 21:16:14 +0200

    fix default method for java 8+ (fixes #104, via #112)

--- atlas-core/src/main/java/io/qameta/atlas/core/internal/DefaultMethodExtension.java
+++ atlas-core/src/main/java/io/qameta/atlas/core/internal/DefaultMethodExtension.java
@@ -2,10 +2,15 @@ package io.qameta.atlas.core.internal;
 
 import io.qameta.atlas.core.api.MethodExtension;
 import io.qameta.atlas.core.util.MethodInfo;
+import org.apache.commons.lang3.JavaVersion;
 
+import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+
+import static org.apache.commons.lang3.SystemUtils.isJavaVersionAtMost;
 
 /**
  * Default method extension.
@@ -20,11 +25,28 @@ public class DefaultMethodExtension implements MethodExtension {
     @Override
     public Object invoke(final Object proxy, final MethodInfo methodInfo, final Configuration config) throws Throwable {
         final Class<?> declaringClass = methodInfo.getMethod().getDeclaringClass();
-        final Constructor<MethodHandles.Lookup> constructor = MethodHandles.Lookup.class
-                .getDeclaredConstructor(Class.class, int.class);
-        constructor.setAccessible(true);
-        return constructor.newInstance(declaringClass, MethodHandles.Lookup.PRIVATE)
-                .unreflectSpecial(methodInfo.getMethod(), declaringClass)
+
+        if (isJavaVersionAtMost(JavaVersion.JAVA_1_8)) {
+            final Constructor<MethodHandles.Lookup> constructor = MethodHandles.Lookup.class
+                    .getDeclaredConstructor(Class.class, int.class);
+            constructor.setAccessible(true);
+            return constructor.newInstance(declaringClass, MethodHandles.Lookup.PRIVATE)
+                    .unreflectSpecial(methodInfo.getMethod(), declaringClass)
+                    .bindTo(proxy)
+                    .invokeWithArguments(methodInfo.getArgs());
+        }
+
+        final MethodHandle methodHandle = MethodHandles.lookup().findSpecial(
+                declaringClass,
+                methodInfo.getMethod().getName(),
+                MethodType.methodType(
+                        methodInfo.getMethod().getReturnType(),
+                        methodInfo.getMethod().getParameterTypes()
+                ),
+                declaringClass
+        );
+
+        return methodHandle
                 .bindTo(proxy)
                 .invokeWithArguments(methodInfo.getArgs());
     }
